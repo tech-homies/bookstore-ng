7523551fc00bdfbbcb4515e5dfffbccabba87798
Benjamin Barbier <benjamin@tech-homies.io>
2025-03-10 23:18:41 +0100

    ✨ add layout nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './layout/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'book-store-ng';
-}
+export class AppComponent {}
