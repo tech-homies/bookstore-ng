0f22b35201297253cab599ababae8637070d7802
Benjamin Barbier <benjamin@tech-homies.io>
2025-03-15 22:49:20 +0100

    🇫🇷 set project configuration to FR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'fr' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+registerLocaleData(localeFr, 'fr');
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
